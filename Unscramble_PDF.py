d3dd594213692451e26b7c0458879926faf991cc
MarcinCichy <marcin-cichy@wp.pl>
2021-11-24 17:49:46 +0000

    Update Unscramble_PDF.py

--- Unscramble_PDF.py
+++ Unscramble_PDF.py
@@ -8,16 +8,15 @@ pdf_path = Path.cwd() / "scrambled.pdf"
 pdf_reader = PdfFileReader(str(pdf_path))
 pdf_writer = PdfFileWriter()
 
-list_of_pages = {}
+dict_of_pages = {}
 for page in pdf_reader.pages:
 	text = page.extractText()
-	index = int(text[0]) - 1
 	if page["/Rotate"] != 0:
 		page.rotateCounterClockwise(page["/Rotate"])
-	list_of_pages[int(text[0])] = page
+	dict_of_pages[int(text[0])] = page
 
-for key in sorted(list_of_pages):
-	pdf_writer.addPage(list_of_pages[key])
+for key in sorted(dict_of_pages):
+	pdf_writer.addPage(dict_of_pages[key])
 
 with Path("Unscrambled.pdf").open(mode="wb") as output_file:
 	pdf_writer.write(output_file)
